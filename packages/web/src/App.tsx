d8cd17995784b3b92cf603c6a72c63f7f07212ff
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-06-04 21:50:47 +0300

    Added stuff.

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { MyNewComponent } from "@my-org/ui";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <MyNewComponent text="hello from shared package" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload. Edit{" "}
+          <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
